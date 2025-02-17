2e52365707e0564d3c4f85eeb0bd790ff266a000
dgag0811 <dgag0811@gmail.com>
2025-02-17 10:51:00 -0500

    methods to implement pairs trading

---
+++ pairs_trading.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+# The data used in the study consists of the daily logarithmic closing prices of Soybean ZS (CBOT), Corn ZC (CBOT),
+# Wheat ZW (CBOT), Rough Rice ZR (CBOT), Oats ZO (CBOT), Live Cattle LE (CME), Lean Hog HE (CME),
+# Cocoa CJ (ICE), Coffee “C” KT (ICE), and Cotton TT (ICE) futures traded on the CBOT, CME and ICE (Figure
+# 2) analyzed.
+
+assets = ["ZS=F", "ZC=F", ]
